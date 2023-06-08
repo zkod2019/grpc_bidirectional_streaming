b859bf77a05c481c20d08f780a4f822032347bbf
Zaya Kod <zkod777@gmail.com>
2023-06-08 15:54:11 +0400

    finished grpc tutorial

---
+++ server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"grpc_bidirectional_streaming/chatserver"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	//assign port
+	Port := os.Getenv("PORT")
+	if Port == "" {
+		Port = "5000" //default Port set to 5000 if PORT is not set in env
+	}
+
+	//init listener
+	listen, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatalf("Could not listen @ %v :: %v", Port, err)
+	}
+	log.Println("Listening @ : " + Port)
+
+	//gRPC server instance
+	grpcserver := grpc.NewServer()
+
+	//register ChatService
+	cs := chatserver.ChatServer{}
+	chatserver.RegisterServicesServer(grpcserver, &cs)
+
+	//grpc listen and serve
+	err = grpcserver.Serve(listen)
+	if err != nil {
+		log.Fatalf("Failed to start gRPC Server :: %v", err)
+	}
+
+}
